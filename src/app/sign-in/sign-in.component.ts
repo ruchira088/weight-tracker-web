5991ce90f92630522f3a1eb25f07d26601fb2f24
Ruchira (Richie) Jayasekara <me@ruchij.com>
2019-10-26 23:40:03 +1100

    Created the sign-in and sign-up page

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from "@angular/core"
-import {AuthenticationService} from "../services/authentication/authentication.service";
-import {Router} from "@angular/router";
+import {Component} from "@angular/core"
+import {AuthenticationService} from "../services/authentication/authentication.service"
+import {Router} from "@angular/router"
 
 @Component({
   selector: "app-sign-in",
@@ -11,11 +11,22 @@ export class SignInComponent {
 
   email = ""
   password = ""
+  loading = false
+  errors = []
 
   constructor(private authenticationService: AuthenticationService, private router: Router) { }
 
   login() {
+    this.errors = []
+    this.loading = true
+
     this.authenticationService.login(this.email, this.password)
-      .subscribe(_ => this.router.navigateByUrl(""))
+      .subscribe(
+        _ => this.router.navigateByUrl(""),
+        ({ error }) => {
+          this.loading = false
+          this.errors = error.errorMessages
+        }
+      )
   }
 }
